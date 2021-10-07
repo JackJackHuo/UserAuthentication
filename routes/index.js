ae49abe2f532e8e98ed223a89fcd11b5fe83d75a
JackHuo <cji4dk40@gmail.com>
2021-10-08 00:14:17 +0800

    refactor: merge route login.js into home.js

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,9 @@
 const express = require('express')
 const router = express.Router()
 const home = require('./modules/home')
-const login = require('./modules/login')
 const signup = require('./modules/signup')
 
 router.use('/', home)
-router.use('/login', login)
 router.use('/signup', signup)
 
 module.exports = router
--- routes/modules/home.js
+++ routes/modules/home.js
@@ -1,9 +1,29 @@
 const express = require('express')
 const router = express.Router()
+const User = require('../../models/user')
 
 router.get('/', (req, res) => {
   const query = req.query
   res.render('index', { message: query.message })
+})
+
+router.post('/', (req, res) => {
+  const { email, password } = req.body
+  User.findOne({ email: email })
+    .lean()
+    .then(user => {
+      if (user) {
+        if (user.password === password) {
+          // res.clearCookie('firstName')
+          res.render('loggedIn', { user: user })
+        } else {
+          res.redirect('/?message=wrong password')
+        }
+      } else {
+        res.redirect('/?message=no user found')
+      }
+    }
+    )
 })
 
 module.exports = router
