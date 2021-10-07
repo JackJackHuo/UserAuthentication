4ded572c3d2fe0cc7ac308d44e0ed89b0f8d9279
JackHuo <cji4dk40@gmail.com>
2021-10-08 03:11:35 +0800

    feat: add cookie for logged-in user & add session to store cookies

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const cookieParser = require('cookie-parser')
 const express = require('express')
 const exphbs = require('express-handlebars')
 const router = require('./routes/index')
@@ -10,7 +11,8 @@ app.set('view engine' , 'hbs')
 
 app.use(express.urlencoded({extended:"true"}))
 app.use(express.static('./public'))
-
+// sign for cookie
+app.use(cookieParser('secretcode'))
 app.use(router)
 
 
--- routes/modules/home.js
+++ routes/modules/home.js
@@ -1,29 +1,62 @@
 const express = require('express')
 const router = express.Router()
 const User = require('../../models/user')
+// session用來存放cookie
+const session = {}
 
+// home page
 router.get('/', (req, res) => {
-  const query = req.query
-  res.render('index', { message: query.message })
+  console.log(session)
+  console.log(cookie)
+  const cookie = req.signedCookies
+  // 檢查session中是否有存放此用戶的cookie
+  if (Object.keys(session).some(sessionId => sessionId === cookie.session_id)){
+    // 從session中抓出登入者的cookie
+    const user = Object.keys(session).find(sessionId => sessionId === cookie.session_id)
+    User.findOne({ _id: session[user].user_id })
+        .lean()
+        .then(user => res.render('loggedIn' , { user: user }))
+    // console.log(session[user].userId)
+  }else{
+    res.render('index')
+  }
 })
 
+// login page
+router.get('/login', (req, res) => {
+  const query = req.query
+  res.render('login', { message: query.message })
+})
+// user login request
 router.post('/', (req, res) => {
   const { email, password } = req.body
   User.findOne({ email: email })
-    .lean()
-    .then(user => {
-      if (user) {
-        if (user.password === password) {
-          // res.clearCookie('firstName')
-          res.render('loggedIn', { user: user })
+      .lean()
+      .then(user => {
+        if (user) {
+          if (user.password === password) {
+            // ex: email => tony@stark.com ;  session_id = tony
+            const session_id = email.split('@')[0]
+            // 把登入者的cookie存入session
+            // session = {tony: {userId: ObjectId("615f2981fd5df83ea5cf8b3e")}}
+            session[session_id] = { user_id: user._id}
+            // 寫入cookie
+            // signed:true代表寫入cookie時有利用app.js裡面的cookieParser('secretcode')上簽章
+            // maxAge設cookie多少millisecond後消失
+            res.cookie('session_id', session_id , {signed:true , path:'/' , maxAge: 5000})
+            res.redirect('/')
+          } else {
+            res.redirect('/login?message=wrong password')
+          }
         } else {
-          res.redirect('/?message=wrong password')
+          res.redirect('/login?message=no user found')
         }
-      } else {
-        res.redirect('/?message=no user found')
-      }
-    }
-    )
+    })
+})
+// user logout request
+router.get('/logout', (req, res) => {
+  res.clearCookie('session_id')
+  res.redirect('/')
 })
 
 module.exports = router
